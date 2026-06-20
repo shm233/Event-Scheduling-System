d9a4dbecded6a57c6232cbfcf6205cd57bcc3699
Safiullah Hossain Mollick <info.safiullah233@gmail.com>
2026-06-20 14:04:41 +0600

    Update settings.py

--- event_scheduling_system/event_scheduling_system/settings.py
+++ event_scheduling_system/event_scheduling_system/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,6 +123,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 AUTH_USER_MODEL = 'event_app.EventUserModel'
 LOGIN_URL = 'sign_in'
