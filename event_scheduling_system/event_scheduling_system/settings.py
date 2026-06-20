28402a68cbe462cea99e45b7766bbe4fcc37401b
Safiullah Hossain Mollick <info.safiullah233@gmail.com>
2026-06-20 13:54:20 +0600

    Update settings.py

--- event_scheduling_system/event_scheduling_system/settings.py
+++ event_scheduling_system/event_scheduling_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(wv6k$m_8co8d_=+&ab(-)$ya*o%^otddy-8s^f!w3+e^r(a+b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
